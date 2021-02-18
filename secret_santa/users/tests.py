88210b90969d4b7182867901e2f5c5c1eeb0a9c7
James Menzies <james.r.menzies@gmail.com>
2021-02-18 20:24:14 +1100

    18 Create Fixtures (#26)
    * workflow added for testing
    
    * workflow should now have .env file
    
    * order of commands change. should work now
    
    * TTY error fixed

--- secret_santa/users/tests.py
+++ secret_santa/users/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import CustomUser
+
+
+class SampleTest(TestCase):
+    fixtures = ["fixtures/sample_user.json"]
+
+    def test_user_exists(self):
+        user = CustomUser.objects.get(pk=1)
+        self.assertEqual("admin@admin.com", user.email)
