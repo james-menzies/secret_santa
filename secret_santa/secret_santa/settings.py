572c9d96c60c7f36610355410c8a7f045d22fc8c
James Menzies <james.r.menzies@gmail.com>
2021-02-18 19:38:33 +1100

    22 Custom User (#25)
    * user app added. custom user created
    
    * user app added. custom user created. migrations working.

--- secret_santa/secret_santa/settings.py
+++ secret_santa/secret_santa/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -104,3 +106,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'users.CustomUser'
---
+++ secret_santa/users/models.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+# Create your models here.
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self, email, password, **kwargs):
+
+        if not email:
+            raise ValueError(_("Email must be set."))
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **kwargs)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **kwargs):
+
+        kwargs.setdefault("is_staff", True)
+        kwargs.setdefault("is_superuser", True)
+        kwargs.setdefault("is_active", True)
+
+        if not kwargs.get("is_staff"):
+            raise ValueError(_("Superuser must be is_staff"))
+        if not kwargs.get("is_superuser"):
+            raise ValueError(_("Superuser must be is_superuser"))
+
+        return self.create_user(email, password, **kwargs)
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return f"<User: {self.email}>"
