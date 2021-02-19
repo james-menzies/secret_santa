7df9506f5a8695970d584110e7c1c3dc7cf703f2
james menzies <james.r.menzies@gmail.com>
2021-02-19 12:08:29 +1100

    created a token path to ensure that media files in a dev env can be served. user registered with admin site.

--- secret_santa/users/views.py
+++ secret_santa/users/views.py
@@ -2,5 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from users.models import CustomUser
+
+
 def login(request):
     return render(request, 'users/login.html')
+
+def view_user(request, id):
+    user = CustomUser.objects.get(pk=id)
+    context = {
+        "user": user,
+    }
+    return render(request, 'users/view_user.html', context=context)
