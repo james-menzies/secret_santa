ef669a34d984f80e2be62ead297a6dc790ccdb4a
james menzies <james.r.menzies@gmail.com>
2021-02-19 11:04:37 +1100

    setting up config for local media storage

--- secret_santa/secret_santa/settings.py
+++ secret_santa/secret_santa/settings.py
@@ -61,6 +61,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.media',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
@@ -107,5 +108,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
 
 AUTH_USER_MODEL = 'users.CustomUser'
