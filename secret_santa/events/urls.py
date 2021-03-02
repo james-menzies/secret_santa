1f9312171e3b0af6a24a7cc74bb1a103f6217119
james menzies <james.r.menzies@gmail.com>
2021-03-02 15:12:36 +1100

    skeleton of site created

--- secret_santa/events/urls.py
+++ secret_santa/events/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from events.views import create, view_all, view_single, give_gift
+from events.views import create, view_all, view_single, give_gift, edit_event
 
 urlpatterns = [
     path('new/', create, name="create_event"),
     path('', view_all, name="view_all_events"),
     path('<int:event_id>/', view_single, name="view_event"),
     path('<int:event_id>/give/', give_gift, name="give_gift" ),
-
+    path('<int:event_id>/edit/', edit_event, name="edit_event")
 ]
--- secret_santa/events/views.py
+++ secret_santa/events/views.py
@@ -4,15 +4,16 @@ from django.shortcuts import render
 # Create your views here.
 def create(request):
 
-    return HttpResponse("The create event page")
+    return render(request, 'events/edit_event.html')
 
 def view_all(request):
-    return HttpResponse("The view all events page")
+    return render(request, 'events/events.html')
 
 def view_single(request, event_id: int):
-
-    return HttpResponse("The view single event page")
+    return render(request, 'events/event_view.html', context={"id": event_id})
 
 def give_gift(request, event_id: int):
+    return render(request, 'events/edit_gift.html', context={"id": event_id})
 
-    return HttpResponse("The give a gift page")
+def edit_event(request, event_id: int):
+    return render(request, 'events/edit_event.html', context={"id": event_id})
