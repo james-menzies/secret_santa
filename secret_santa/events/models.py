3108506f233f8fd7ae2254f1ce500c5d9ac0b959
james menzies <james.r.menzies@gmail.com>
2021-03-02 14:12:21 +1100

    models working as expected

--- secret_santa/events/models.py
+++ secret_santa/events/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=1000, null=True, blank=True)
+    image = models.ImageField(upload_to='event_pictures', null=True, blank=True)
+    time = models.DateTimeField()
+    active = models.BooleanField(default=False)
+    revealed = models.BooleanField(default=True)
+
+
+
+class Gift(models.Model):
+
+    message = models.CharField(max_length=100)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE,
+                              related_name='gifts',
+                              related_query_name='gifts')
